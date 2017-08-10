5218ddf495daaebb887885c1a6f0db5994c7b68f
Jason Orcutt <jorcutt@zynga.com>
2017-08-10 17:34:14 +0000

    NO-TICKET: initial checkin

---
+++ src/Zynga/Framework/Environment/SuperGlobals/V1/SuperGlobals.hh
@@ -0,0 +1,136 @@
+<?hh // partial
+
+namespace Zynga\Framework\Environment\SuperGlobals\V1;
+
+class SuperGlobals {
+  const string SCRIPT_FILE = 'SCRIPT_FILENAME';
+  const string SCRIPT_URI = 'SCRIPT_URI';
+  const string SCRIPT_ARGV = 'argv';
+  const string SCRIPT_ARGC = 'argc';
+  const string DEFAULT_HTTP_BODY_FILE = 'php://input';
+
+  private static string $_bodyFilePath = self::DEFAULT_HTTP_BODY_FILE;
+
+  public static function setScriptUri(string $uri): bool {
+    $_SERVER[self::SCRIPT_URI] = $uri;
+    return true;
+  }
+
+  public static function getScriptUri(): string {
+    if (key_exists(self::SCRIPT_URI, $_SERVER)) {
+      return $_SERVER[self::SCRIPT_URI];
+    }
+    return '';
+  }
+
+  public static function clearScriptUri(): bool {
+    if (key_exists(self::SCRIPT_URI, $_SERVER)) {
+      unset($_SERVER[self::SCRIPT_URI]);
+      return true;
+    }
+    return false;
+  }
+
+  public static function setScriptName(string $value): bool {
+    $_SERVER[self::SCRIPT_FILE] = $value;
+    return true;
+  }
+
+  public static function getScriptName(): string {
+    if (key_exists(self::SCRIPT_FILE, $_SERVER)) {
+      return $_SERVER[self::SCRIPT_FILE];
+    }
+    return '';
+  }
+
+  public static function unsetScriptName(): bool {
+    if (key_exists(self::SCRIPT_FILE, $_SERVER)) {
+      unset($_SERVER[self::SCRIPT_FILE]);
+    }
+    return true;
+  }
+
+  public static function setScriptCommandLineArguments(
+    array<string> $args,
+  ): bool {
+    $_SERVER[self::SCRIPT_ARGV] = $args;
+    $_SERVER[self::SCRIPT_ARGC] = count($args);
+    return true;
+  }
+
+  public static function getScriptCommandLineArguments(): mixed {
+    if (key_exists(self::SCRIPT_ARGV, $_SERVER)) {
+      return $_SERVER[self::SCRIPT_ARGV];
+    }
+    return null;
+  }
+
+  public static function clearScriptCommandLineArguments(): bool {
+    if (key_exists(self::SCRIPT_ARGV, $_SERVER)) {
+      unset($_SERVER[self::SCRIPT_ARGV]);
+    }
+    if (key_exists(self::SCRIPT_ARGC, $_SERVER)) {
+      unset($_SERVER[self::SCRIPT_ARGC]);
+    }
+    return true;
+  }
+
+  public static function unsetRequestPairing(string $key): bool {
+    if (key_exists($key, $_REQUEST)) {
+      unset($_REQUEST[$key]);
+    }
+    return true;
+  }
+
+  public static function clearRequest(): bool {
+    $_REQUEST = array();
+    return true;
+  }
+
+  public static function setRequestPairing(string $key, mixed $value): bool {
+    $_REQUEST[$key] = $value;
+    return true;
+  }
+
+  public static function getServerAsMap(): Map<string, mixed> {
+    return new Map($_SERVER);
+  }
+
+  public static function getGetAsMap(): Map<string, mixed> {
+    return new Map($_GET);
+  }
+
+  public static function getPostAsMap(): Map<string, mixed> {
+    return new Map($_POST);
+  }
+
+  public static function getRequestAsMap(): Map<string, mixed> {
+    return new Map($_REQUEST);
+  }
+
+  public static function getBodyFilePath(): string {
+    return self::$_bodyFilePath;
+  }
+
+  public static function setBodyFilePath(string $file): bool {
+    self::$_bodyFilePath = $file;
+    return true;
+  }
+
+  public static function getRequestBodyAsMap(): Map<string, mixed> {
+
+    $rawBody = file_get_contents(self::$_bodyFilePath);
+
+    $bodyAsArray = json_decode($rawBody, true);
+
+    if (is_array($bodyAsArray) === true) {
+      return new Map($bodyAsArray);
+    }
+
+    $blank = Map {};
+
+    return $blank;
+
+  }
+
+}
---
+++ src/Zynga/Framework/Environment/SuperGlobals/V1/SuperGlobalsTest.hh
@@ -0,0 +1,114 @@
+<?hh //strict
+
+namespace Zynga\Framework\Environment\SuperGlobals\V1;
+
+use Zynga\Framework\Testing\TestCase\V2\Base as TestCase;
+use Zynga\Framework\Environment\SuperGlobals\V1\SuperGlobals;
+
+class SuperGlobalsTest extends TestCase {
+  public string $_scriptName;
+
+  public function __construct(string $name) {
+
+    parent::__construct($name);
+
+    $this->_scriptName = '';
+
+  }
+
+  public function setUp(): void {
+    $this->_scriptName = SuperGlobals::getScriptName();
+  }
+
+  public function tearDown(): void {
+    SuperGlobals::setScriptName($this->_scriptName);
+  }
+
+  public function test_bodyFilePath(): void {
+    $this->assertEquals(
+      SuperGlobals::DEFAULT_HTTP_BODY_FILE,
+      SuperGlobals::getBodyFilePath(),
+    );
+    $this->assertTrue(SuperGlobals::setBodyFilePath('/etc/passwd'));
+    $this->assertEquals('/etc/passwd', SuperGlobals::getBodyFilePath());
+    $this->assertTrue(
+      SuperGlobals::setBodyFilePath(SuperGlobals::DEFAULT_HTTP_BODY_FILE),
+    );
+  }
+
+  public function testValid(): void {
+    $this->assertEquals(Map::class, get_class(SuperGlobals::getGetAsMap()));
+    $this->assertEquals(Map::class, get_class(SuperGlobals::getPostAsMap()));
+    $this->assertEquals(
+      Map::class,
+      get_class(SuperGlobals::getRequestAsMap()),
+    );
+
+    $key = 'someRandoKey';
+    $value = mt_rand(1, 10000);
+
+    $requestVars = SuperGlobals::getRequestAsMap();
+    $this->assertFalse($requestVars->containsKey($key));
+
+    $requestVars = null;
+
+    $this->assertTrue(SuperGlobals::setRequestPairing($key, $value));
+
+    $requestVars = SuperGlobals::getRequestAsMap();
+    $this->assertTrue($requestVars->containsKey($key));
+    $this->assertEquals($value, $requestVars->get($key));
+
+    $requestVars = null;
+
+    SuperGlobals::unsetRequestPairing($key);
+    $requestVars = SuperGlobals::getRequestAsMap();
+    $this->assertFalse($requestVars->containsKey($key));
+
+    $serverVars = SuperGlobals::getServerAsMap();
+    $this->assertTrue($serverVars instanceof Map);
+
+  }
+
+  public function testScriptNameNotFound(): void {
+    $this->assertTrue(SuperGlobals::unsetScriptName());
+    $this->assertEquals('', SuperGlobals::getScriptName());
+  }
+
+  public function testGetScriptCommandLineArguments(): void {
+
+    $scriptArgs = SuperGlobals::getScriptCommandLineArguments();
+
+    $this->assertTrue(is_array($scriptArgs));
+
+    // clear the arguments
+    $this->assertTrue(SuperGlobals::clearScriptCommandLineArguments());
+    $this->assertEquals(null, SuperGlobals::getScriptCommandLineArguments());
+
+    // reset the args
+    if (is_array($scriptArgs)) {
+
+      SuperGlobals::setScriptCommandLineArguments($scriptArgs);
+
+      $newValue = SuperGlobals::getScriptCommandLineArguments();
+
+      $this->assertEquals(count($scriptArgs), count($newValue));
+
+    }
+
+  }
+
+  public function testGetScriptUri(): void {
+
+    $currentUri = SuperGlobals::getScriptUri();
+
+    SuperGlobals::clearScriptUri();
+    $this->assertEquals('', SuperGlobals::getScriptUri());
+    SuperGlobals::setScriptUri('http://www.cnn.com');
+    $this->assertEquals('http://www.cnn.com', SuperGlobals::getScriptUri());
+    $this->assertTrue(SuperGlobals::clearScriptUri());
+
+    $this->assertTrue(SuperGlobals::setScriptUri($currentUri));
+
+  }
+
+}

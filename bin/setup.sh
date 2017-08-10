12daa9f039e00e8851cdc037b2e4b8331796d7c3
Jason Orcutt <jorcutt@zynga.com>
2017-08-10 17:00:32 +0000

    NO-TICKET: cleaning up initial checkin, works but is missing deps

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,33 +5,19 @@ add_to_git_ignore()
   gitFile="$1/.gitignore"
   ignoreTarget=$2
 
+  touch $gitFile;
+
   if [ ! -f $gitFile ]; then
-    echo "gitFile=$gitFile not present"
-    return 255;
+    echo "  gitIgnore - gitFile=$gitFile not present"
+    return;
   fi
 
   if grep -q $ignoreTarget $gitFile; then
-    echo "$ignoreTarget - already ignored"
+    echo "  gitIgnore - $ignoreTarget - already ignored"
   else
-    echo "$ignoreTarget - adding to ignoreFile=$gitFile"
+    echo "  gitIgnore - $ignoreTarget - adding to ignoreFile=$gitFile"
     echo $ignoreTarget >> $gitFile
   fi
-
-  return 0;
-
-}
-
-checksum_file()
-{
-  targetFile=$1
-
-  if [ ! -f $targetFile ]; then
-    return "";
-  fi
-
-  checksum=`md5sum $targetFile | awk '{print $1'}`
-
-  return $checksum;
 
 }
 
@@ -41,18 +27,24 @@ copy_from_skeleton()
   sourceFile=$2
   destFile=$3
 
-  sourceChecksum = checksum_file $sourceFile;
-  destChecksum = checksum_file $destFile;
-
-  if [ $sourceChecksum -eq $destChecksum ]; then
-    echo "$destFile - aok"
-  else 
-    echo "sourceFile=$sourceFile and destFile=$destFile do not match"
-    echo "sourceChecksum=$sourceChecksum"
-    echo "destChecksum=$destChecksum"
+  sourceChecksum=""
+  if [ -f $sourceFile ]; then
+    sourceChecksum=`md5sum "$sourceFile" | awk '{print $1'}`;
+  fi
+  
+  destChecksum=""
+  if [ -f $destFile ]; then
+    destChecksum=`md5sum "$destFile" | awk '{print $1'}`;
   fi
 
-  add_to_git_ignore $projectRoot $destFile
+  if [ "$sourceChecksum" == "$destChecksum" ]; then
+    echo "  CopyFromSkeleton - destFile=$destFile - aok"
+  else 
+    echo "  CopyFromSkeleton - sourceFile=$sourceFile and destFile=$destFile do not match"
+    echo "  CopyFromSkeleton - sourceChecksum=$sourceChecksum"
+    echo "  CopyFromSkeleton -  destChecksum=$destChecksum"
+    cp $sourceFile $destFile
+  fi
 
 }
 
@@ -61,28 +53,34 @@ setup_dir()
   targetDir=$1
 
   if [ ! -d $targetDir ]; then
-    echo "Attempting to create targetDir=$targetDir"
+    echo "  setupDir - Attempting to create targetDir=$targetDir"
     mkdir -p $targetDir
   else 
-    echo "targetDir=$targetDir - aok"
-    return 0
+    echo "  setupDir - targetDir=$targetDir - aok"
+    return
   fi
 
   if [ ! -d $targetDir ]; then
-    echo "Failed to setup targetDir=$targetDir"
-    return 255;
+    echo "  setupDir  - Failed to setup targetDir=$targetDir"
+    exit 255;
   fi
 
 }
 
+echo "STARTUP"
+
 project_root="."
+
+echo "  projectRoot=$project_root"
 
 ## 
 # Exclude our standard directories we care about.
 ##
+echo "Setting up top level directories"
 setup_dir "$project_root/tmp"
 setup_dir "$project_root/vendor"
 
+echo "Excluding top level directories from git"
 add_to_git_ignore $project_root "vendor";
 add_to_git_ignore $project_root "tmp";
 
@@ -94,5 +92,9 @@ echo "Copying in files from project skeleton"
 # Copy over our makefile
 copy_from_skeleton \
   $project_root \
-  "$project_root/vendor/zynga/zynga-hhvm-moduledev/skeleton/Makefile" \
+  "$project_root/vendor/zynga/zynga-hhvm-moduledev/src/skeleton/Makefile" \
   "$project_root/Makefile"
+
+add_to_git_ignore $project_root "Makefile"
+
+echo "DONE"

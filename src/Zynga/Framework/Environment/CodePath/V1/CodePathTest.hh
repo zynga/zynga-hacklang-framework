e8d0c61f133ea939f4bb774195377940702fb27e
Jason Orcutt <jorcutt@zynga.com>
2017-08-11 20:03:50 +0000

    NO-TICKET: initial checkin

---
+++ src/Zynga/Framework/Environment/CodePath/V1/CodePathTest.hh
@@ -0,0 +1,46 @@
+<?hh //strict
+
+namespace Zynga\Framework\Environment\CodePath\V1;
+
+use Zynga\Framework\Environment\CodePath\V1\CodePath;
+use Zynga\Framework\Testing\TestCase\V2\Base as TestCase;
+
+class CodePathTest extends TestCase {
+  private string $_currentRoot = '';
+
+  public function captureCurrentRoot(): void {
+    $this->_currentRoot = CodePath::getRoot();
+  }
+
+  public function restoreCurrentRoot(): void {
+    CodePath::setRoot($this->_currentRoot);
+  }
+
+  /**
+   * Do we return the right path for our code
+   */
+  public function testGetExistingRoot(): void {
+    $expected =
+      dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))));
+    $value = CodePath::getRoot();
+    $this->assertEquals($expected, $value);
+  }
+
+  /**
+   * Are we able to set a new root and return it
+   */
+  public function testChangeRoot(): void {
+
+    $this->captureCurrentRoot();
+
+    $expected = '/monkey';
+
+    CodePath::setRoot($expected);
+    $value = CodePath::getRoot();
+
+    $this->assertEquals($expected, $value);
+
+    $this->restoreCurrentRoot();
+
+  }
+}

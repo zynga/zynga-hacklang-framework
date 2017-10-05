3d7c38b6e187e9671b2e62607f04bd1b71edac5e
Jason Orcutt <jorcutt@zynga.com>
2017-10-05 22:33:54 +0000

    travis build inits

--- .build.sh
+++ .build.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+set -ex
+
+# update the environment via apt
+apt-get update -y
+
+# install modules we need to use to test with.
+apt-get install -y wget curl git
+
+# Bring the latest composer into the environment
+curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
+
+# turn off the jit
+echo hhvm.jit=0 >> /etc/hhvm/php.ini
+
+# show the version of hhvm
+hhvm --version
+
+# swap to the source directory
+cd /var/source
+
 # run composer
 composer update
 

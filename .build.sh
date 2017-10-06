f06ee0bff1b24e654b1fff9807932bd1caaded7b
Jason Orcutt <jorcutt@zynga.com>
2017-10-06 21:51:08 +0000

    adding mysql dep work in

--- .build.sh
+++ .build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+setup_composer()
+{
+
+  composerFile=/usr/local/bin/composer
+
+  if [ ! -f $composerFile ]; then
+    # install the composer binary to /usr/local/bin
+    curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
+  fi
+
+  if [ -f $composerFile ]; then
+    echo "Failed to install composer to $composerFile";
+    exit 255;
+  fi
+
+}
+
 set -ex
 
 # update the environment via apt
@@ -9,7 +26,7 @@ apt-get update -y
 apt-get install -y wget curl git make time
 
 # Bring the latest composer into the environment
-curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
+setup_composer;
 
 # turn off the jit
 echo hhvm.jit=0 >> /etc/hhvm/php.ini
@@ -23,7 +40,7 @@ cd /var/source
 # run composer
 composer update
 
-if [ $? -ne 0 ]; then 
+if [ $? -ne 0 ]; then
   echo "Composer failed to update"
   exit $?
 fi
@@ -34,14 +51,14 @@ composer show
 # composer is available now run the vendor setup script
 ./bin/setup.sh
 
-if [ $? -ne 0 ]; then 
+if [ $? -ne 0 ]; then
   echo "Dev environment failed to configure properly"
   exit $?
 fi
 
 make test
 
-if [ $? -ne 0 ]; then 
+if [ $? -ne 0 ]; then
   echo "Make test failure"
   exit $?
 fi
---
+++ tests/sql/mysql/create_database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE `phpunit`;
+
+CREATE TABLE `phpunit`.`phpunit` (
+  `unit_test_stamp` int(11) NOT NULL,
+  `int_data` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8

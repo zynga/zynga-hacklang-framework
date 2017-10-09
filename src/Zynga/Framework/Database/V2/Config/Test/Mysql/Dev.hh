07bf2f9d9b4121f2e2dfd35ab0a0111adbc919e1
Jason Orcutt <jorcutt@zynga.com>
2017-10-09 21:06:16 +0000

    renamed database sql, bringing in protobuf testing binaries

---
+++ src/Zynga/Framework/Database/V2/Config/Test/Mysql/Dev.hh
@@ -0,0 +1,8 @@
+<?hh // strict
+
+namespace Zynga\Framework\Database\V2\Config\Test\Mysql;
+
+use Zynga\Framework\Database\V2\Config\Test\Mysql\Base as ConfigBase;
+
+class Dev extends ConfigBase {
+}
---
+++ src/Zynga/Framework/Database/V2/Config/Test/Mysql/Staging.hh
@@ -0,0 +1,8 @@
+<?hh // strict
+
+namespace Zynga\Framework\Database\V2\Config\Test\Mysql;
+
+use Zynga\Framework\Database\V2\Config\Test\Mysql\Base as ConfigBase;
+
+class Staging extends ConfigBase {
+}

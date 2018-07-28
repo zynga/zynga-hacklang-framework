8e869db47ac8c4eea8b09a5773ae8e39b491e712
Jason Orcutt <jorcutt@zynga.com>
2018-07-28 16:41:37 -0700

    moving to utilizing the pre-initialized docker image

--- .build.sh
+++ .build.sh
@@ -1,63 +1,17 @@
 #!/bin/bash
 
-setup_composer()
-{
-
-  composerFile=/usr/local/bin/composer
-
-  if [ ! -f $composerFile ]; then
-    # install the composer binary to /usr/local/bin
-    curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
-  fi
-
-  if [ ! -f $composerFile ]; then
-    echo "Failed to install composer to $composerFile";
-    exit 255;
-  fi
-
-}
-
 set -ex
 
-# update the environment via apt
-apt-get update -y
-
-# install modules we need to use to test with.
-apt-get install -y wget curl git make time
-
-export DEBIAN_FRONTEND=noninteractive
-
-# stand up a memcached on the image
-apt-get install -y memcached
 /etc/init.d/memcached start
-
-# install mysql-server
-apt-get install -y mysql-server
 /etc/init.d/mysql start
+/etc/init.d/postgresql start
 
-# load up our test database
-mysql < /var/source/tests/sql/mysql/create_test_database.sql
+# dump out the schema as it stands
 mysql -e 'SHOW DATABASES'
 mysql -e 'SHOW TABLES' phpunit
 
-# install postgresql
-apt-get install -y postgresql postgresql-contrib
-/etc/init.d/postgresql start
-
-# install the database user.
-su postgres -c 'psql < /var/source/tests/sql/postgresql/create_test_database.sql'
-
-# setup the postgres password into the .pgpass property file.
-echo "localhost:5432:phpunit:zframework:i-am-a-walrus" > ~/.pgpass
-chmod 0600 ~/.pgpass
-
+# dump out the schema as it stands
 echo '\d' | psql --user=zframework --host=localhost phpunit
-
-# Bring the latest composer into the environment
-setup_composer;
-
-# turn off the jit
-echo hhvm.jit=0 >> /etc/hhvm/php.ini
 
 # show the version of hhvm
 hhvm --version

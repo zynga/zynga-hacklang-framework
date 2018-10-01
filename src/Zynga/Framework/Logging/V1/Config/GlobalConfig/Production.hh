951db09ab9e94d702f1ee74152e8c3dc78ec3aa6
czablocki <czablocki@zynga.com>
2018-10-01 19:06:18 +0000

    NO-TICKET: Adding Staging and Production GlobalConfigs for Logging

---
+++ src/Zynga/Framework/Logging/V1/Config/GlobalConfig/Production.hh
@@ -0,0 +1,26 @@
+<?hh // strict
+
+namespace Zynga\Framework\Logging\V1\Config\GlobalConfig;
+
+use Zynga\Framework\Logging\V1\Adapter\ErrorLog;
+use Zynga\Framework\Logging\V1\Config\Base as ConfigBase;
+use Zynga\Framework\Logging\V1\Level;
+
+class Production extends ConfigBase {
+
+  public function init(): bool {
+    $errorLog = new ErrorLog();
+    $this->addAdapter($errorLog);
+    $this->unsetLogLevel(Level::DEBUG);
+    $this->setLogLevel(Level::INFO);
+    $this->setLogLevel(Level::WARNING);
+    $this->setLogLevel(Level::ERROR);
+    $this->setLogLevel(Level::CRITICAL);
+    return true;
+  }
+
+  public function getDriver(): string {
+    return 'Tee';
+  }
+
+}

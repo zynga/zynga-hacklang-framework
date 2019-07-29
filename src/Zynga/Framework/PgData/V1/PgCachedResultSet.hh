0090ed1624368fbeec7b6445bc07353acd23e391
Sajini Manikandan <smanikandan@zynga.com>
2019-07-29 15:53:51 -0700

    NO-TICKET: added md5 to the model name that was added to the checksum

--- src/Zynga/Framework/PgData/V1/PgCachedResultSet.hh
+++ src/Zynga/Framework/PgData/V1/PgCachedResultSet.hh
@@ -41,8 +41,9 @@ class PgCachedResultSet<Tv as TypeInterface> extends Base<Tv> {
     $rawType = $this->_rawType;
     $typeChecksum = md5($this->_rawType);
     $whereChecksum = $this->_where->createWhereChecksum();
+    $modelChecksum = md5($this->_model);
 
-    $this->_checksum = $typeChecksum.'|'.$whereChecksum.'|'.$this->_model;
+    $this->_checksum = $typeChecksum.'|'.$whereChecksum.'|'.$modelChecksum;
 
     return $this->_checksum;
 
